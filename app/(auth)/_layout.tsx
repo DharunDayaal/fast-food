3449488262192d98180e2ca4528df795c66279a5
dharun <dharundayaal@gmail.com>
2025-07-08 12:31:30 +0530

    Added the files and folder structure

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,13 @@
+import { Slot } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function _Layout() {
+    return (
+        <SafeAreaView>
+            <Text>Auth _Layout</Text>
+            <Slot />
+        </SafeAreaView>
+    );
+}
